70a15d7cd462c7b40f89007ef3bca66e2bb01799
Susanta Kumar Das <susantakumardas@mindtechlabs.com>
2018-07-26 12:28:58 +0530

    update
    update

--- application/modules/recruiter/views/common/recruiter-bottom.php
+++ application/modules/recruiter/views/common/recruiter-bottom.php
@@ -1,7 +1,7 @@
         <!-- Bootstrap Core Js -->
         <script src="<?php echo config_item('assets_dir');?>plugins/bootstrap/js/bootstrap.js"></script>
         <!-- Select Plugin Js -->
-        <script src="<?php echo config_item('assets_dir');?>plugins/bootstrap-select/js/bootstrap-select.js"></script>
+        <!-- <script src="<?php echo config_item('assets_dir');?>plugins/bootstrap-select/js/bootstrap-select.js"></script> -->
         <!-- Slimscroll Plugin Js -->
         <script src="<?php echo config_item('assets_dir');?>plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
         <!-- Waves Effect Plugin Js -->
@@ -10,5 +10,14 @@
         <script src="<?php echo config_item('assets_dir');?>js/admin.js"></script>
         <!-- Demo Js -->
         <script src="<?php echo config_item('assets_dir');?>js/demo.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {
+                var pageId = $('#page').val();
+                if(pageId!=undefined || pageId!=''){
+                    $('.ourmenu').removeClass('active');
+                    $('#'+pageId).addClass('active');
+                }
+            });
+        </script>
     </body>
 </html>
